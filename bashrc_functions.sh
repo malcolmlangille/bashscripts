d8702cc86287527c7313a9125e2499b52f0c5113
Malcolm Langille <malcolmlangille@gmail.com>
2025-02-03 09:52:23 -0500

    Fixed the .bashrc file

--- bashrc_functions.sh
+++ bashrc_functions.sh
@@ -1,3 +1,21 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        ./etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:"]]
+then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+# Used to grep the history for a word and highlight it
 historyg() {
     if [[ -z "$1" ]]; then
         echo "Usage: hgrep <search_word>"
@@ -6,23 +24,17 @@ historyg() {
     history | grep --color=always -i "$1"
 }
 
-historyx() {
-    if [[ -z "$1" ]]; then
-        echo "Usage: historyx <history_number>"
-        return 1
-    fi
+# Use this to get the command at history location 123
+# !123:p to get command. Push up.
 
-    # Extract command from history
-    local cmd=$(history | awk -v num="$1" '$1 == num {sub(/^[0-9]+[[:space:]]*/, "", $0); print $0}')
-
-    # If command found, populate it in the prompt
-    if [[ -n "$cmd" ]]; then
-        echo "$cmd"  # Show the command
-        READLINE_LINE="$cmd"  # Populate it in the command prompt
-        READLINE_POINT=${#cmd}  # Move cursor to end
-    else
-        echo "No command found for history number $1"
-        return 1
-    fi
+# Use this command to easily check in to git
+function lazygit() {
+        git add -A
+        git commit -m "$1"
+        git push
 }
+
+# Add idea /bin to the path to allow the opening of files in the editor using the idea command on the commandline
+export PATH="/idea-server/bin:$PATH"
+
 
