5a75b8c079ec9622c9dfae43fdba65db30979894
Malcolm Langille <malcolmlangille@gmail.com>
2025-02-06 14:34:19 -0500

    Added prompt changes

--- bashrc_functions.sh
+++ bashrc_functions.sh
@@ -35,7 +35,28 @@ function lazygit() {
         git push
 }
 
+# Use this command to set 
+function setgitemail() {
+        git add -A
+        git commit -m "$1"
+        git push
+}
+
 # Add idea /bin to the path to allow the opening of files in the editor using the idea command on the commandline
 export PATH="/idea-server/bin:$PATH"
+
+# Source the Git prompt script from the specified location.
+. /usr/share/git-core/contrib/completion/git-prompt.sh
+
+# Define color variables (optional)
+GREEN="\[\033[01;32m\]"
+YELLOW="\[\033[01;33m\]"
+RESET="\[\033[00m\]"
+
+# Customize the PS1 prompt:
+# - \w prints the full working directory (e.g. /projects/scripts).
+# - __git_ps1 prints the current git branch if inside a git repository.
+export PS1="${GREEN}\w${RESET}\$(__git_ps1 ' (${YELLOW}%s${RESET})') \$ "
+
 
 
