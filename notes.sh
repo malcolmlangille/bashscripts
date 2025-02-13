b946a11f12d8e555ad90ef9364754dac247c19ca
Malcolm Langille <malcolmlangille@gmail.com>
2025-02-13 11:31:40 -0500

    Updated notes

--- notes.sh
+++ notes.sh
@@ -3,3 +3,12 @@ java -jar keycloak-0.0.1-SNAPSHOT.jar \
   -Dkeycloak.migration.provider=dir \
   -Dkeycloak.migration.dir=./keycloak-export \
   -Dkeycloak.migration.usersExportStrategy=REALM_FILE
+
+bootRun {
+    systemProperties = [
+        'keycloak.migration.action': 'export',
+        'keycloak.migration.provider': 'dir',
+        'keycloak.migration.dir': './export-directory',
+        'keycloak.migration.usersExportStrategy': 'REALM_FILE'
+    ]
+}
